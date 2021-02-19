65daa5727dbcf57c30ae118937da17e8c8a9b029
khushbukella <46668377+khushbukella@users.noreply.github.com>
2021-02-19 10:01:03 +0530

    Update calc.py

--- sources/calc.py
+++ sources/calc.py
@@ -1,6 +1,13 @@
 def add2(arg1, arg2):
-    return arg1+ arg2
+    if (arg1>0 and arg2>0): 
+        return arg1+ arg2
+    return "Wrong Input"
 def sub2(arg1, arg2):
-    return arg1-arg2
+    if (arg1>0 and arg2>0): 
+        return arg1-arg2
+    return "Wrong Input"
+
 def pro2(arg1, arg2):
-    return arg1*arg2
+    if (arg1>0 and arg2>0): 
+        return arg1*arg2
+    return "Wrong Input"
