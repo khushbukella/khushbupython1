4473df6c3496128c256bb347e52bfddf11231c57
khushbukella <46668377+khushbukella@users.noreply.github.com>
2021-02-19 10:03:32 +0530

    Update test_calc.py

--- sources/test_calc.py
+++ sources/test_calc.py
@@ -4,7 +4,7 @@ import calc
 class TestCalc(unittest.TestCase):
 
     def test_add(self):
-        result = calc.add2(1, -5)
+        result = calc.add2(1, 5)
         self.assertEqual(result, 6)
     def test_sub(self):
         result = calc.sub2(5, 2)
